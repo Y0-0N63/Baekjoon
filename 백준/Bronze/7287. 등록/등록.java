d731043c8f94c03b168d32f28c79d0571797821f
Boyun Choi <144354615+Y0-0N63@users.noreply.github.com>
2025-09-17 14:01:39 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int correct = 10;
+		String id = "boyun1625";
+		
+		System.out.println(correct);
+		System.out.println(id);
+	}
+
+}

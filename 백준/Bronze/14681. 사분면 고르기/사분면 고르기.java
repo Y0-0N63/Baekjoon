e7f78337dec4890c1d611cb998a8c6673360b85f
Boyun Choi <144354615+Y0-0N63@users.noreply.github.com>
2025-09-22 17:30:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		if((-1000 <= x && x <= 1000) && (-1000 <= y && y <= 1000) && x != 0 && y != 00) {
+			if(x > 0 && y > 0) {
+				System.out.println("1");
+			} else if (x < 0 && y > 0) {
+				System.out.println("2");
+			} else if (x < 0 && y < 0) {
+				System.out.println("3");
+			} else if (x > 0 && y < 0) {
+				System.out.println("4");
+			}
+		}
+	}
+
+}

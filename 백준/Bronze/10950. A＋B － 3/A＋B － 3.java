00ece6c52dce8b7f75aac379374842bff44954ec
Boyun Choi <144354615+Y0-0N63@users.noreply.github.com>
2025-09-23 17:41:56 +0900

    [Bronze V] Title: A+B - 3, Time: 192 ms, Memory: 17896 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int test = sc.nextInt();
+		int arr[] = new int[test];
+
+		for (int i = 0; i < test; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			arr[i] = a + b;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
